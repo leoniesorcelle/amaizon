c739222b0f1293654b99d76ae1d9ffe5115ad62f
Alpathrn <36441490+Alpathrn@users.noreply.github.com>
2019-04-30 13:36:54 +0200

    Add files via upload

---
+++ Amaizon_v0.1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+$page = (object)[
+	'title' => 'Amaizon - Home',
+	'selected' => 'home'
+];
+include('head.php');
+?>
+
+<body scroll="no">
+	<?php
+	include('menu.php')
+	?>
+	<div id="content" class="container-fluid">
+		<div class="row">
+			<div class="col-sm">
+			</div>
+			<div class="col-sm">
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
